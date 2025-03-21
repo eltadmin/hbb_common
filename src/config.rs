851f3e9df73e60ac81b975996c2e2afc4d737b55
dongepi <sysadmin@eltrade.com>
2025-03-21 08:38:21 +0200

    safas
    fsasa

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["46.55.253.3"];
+pub const RS_PUB_KEY: &str = "X8xa64c3b3tbiUnLcLKcQdujdFc7lVhY3n2sUu92sEg=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
